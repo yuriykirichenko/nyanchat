0ddda1c66f792c3fac270a72fb44ab16064c5c74
KoalaLoveTree <kiritokun2133@gmail.com>
2016-10-02 14:41:38 +0300

    Translate interface for English and create "Exit" operation. Initialized all elements of interface in "Controller.java" class.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,20 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Parent;
+        import javafx.scene.Scene;
+        import javafx.scene.image.Image;
+        import javafx.stage.Stage;
+        import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         Parent root = FXMLLoader.load(getClass().getResource("Registration.fxml"));
-        //primaryStage.setTitle("N.CHAT");
+        primaryStage.setTitle("N.CHAT");
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("nyan-cat-face.png")));
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 600, 450));
